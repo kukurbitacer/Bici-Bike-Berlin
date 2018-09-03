261f3feb4c2be4f90f002a9b386832d155bee9e8
camille raiser <camille.raiser@hotmail.fr>
2018-09-03 23:30:49 +0200

    add styling and yellow wrapper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @tours = Tour.where(active: true, first_page: true )
   end
 end
