49e1f89410bc126f02ce66eea5caf6c062605219
camille raiser <camille.raiser@hotmail.fr>
2018-09-05 11:57:16 +0200

    fix routes for default english redirection to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  scope '(:locale)', locales: /de/ do
+
+  scope '(:locale)', locale: /de/ do
     root to: 'pages#home'
     resources :tours, only: [:index, :show]
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
