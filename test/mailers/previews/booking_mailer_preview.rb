39f3f79456cd867e2864d6e57cfbcfdc87576974
camille raiser <camille.raiser@hotmail.fr>
2018-09-14 19:44:57 +0200

    frame for user email template

--- test/mailers/previews/booking_mailer_preview.rb
+++ test/mailers/previews/booking_mailer_preview.rb
@@ -1,12 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/booking_mailer
 class BookingMailerPreview < ActionMailer::Preview
   def booking_user
-    booking = Booking.first
+    booking = Booking.last
     BookingMailer.booking_user(booking)
   end
 
   def booking_admin
-    booking = Booking.first
+    booking = Booking.last
     BookingMailer.booking_admin(booking)
   end
 end
