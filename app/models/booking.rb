18dc523ec1f6f992e6a32061044796c956bf4a53
camille raiser <camille.raiser@hotmail.fr>
2018-09-14 12:14:47 +0200

    add bike number to form and js animation for appearing and disapearing

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,5 +5,6 @@ class Booking < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "Not a valid email" }
   validates :booking_date, presence: true
-  validates :person_attending, presence: true, numericality: { only_integer: true }
+  validates :person_attending, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :bike_number, numericality: { only_integer: true, greater_than_or_equal_to: 0  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_101556) do
+ActiveRecord::Schema.define(version: 2018_09_14_090153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_09_12_101556) do
     t.bigint "tour_id"
     t.integer "person_attending"
     t.boolean "bike", default: false
+    t.integer "bike_number"
     t.index ["tour_id"], name: "index_bookings_on_tour_id"
   end
 
