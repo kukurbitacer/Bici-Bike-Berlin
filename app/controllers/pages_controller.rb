2d2405ac93322d0dd14b2bd7bcc011da94f9d34e
camille raiser <camille.raiser@hotmail.fr>
2018-09-17 18:30:51 +0200

    information wrapper styling and remove card animation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @tours = Tour.where(active: true, first_page: true )
+    @tours = Tour.where(active: true)
   end
 end
