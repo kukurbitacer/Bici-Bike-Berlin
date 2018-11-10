73122a4ddf6b6bc4ad4830dbb3dc05a38d4b3146
camille raiser <camille.raiser@hotmail.fr>
2018-11-10 12:04:10 +0100

    change email settings

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -9,7 +9,7 @@ class BookingMailer < ApplicationMailer
   def booking_admin(booking)
     @booking = booking
 
-    mail(to: 'camille.raiser@hotmail.fr', subject: 'Neue Tour Buchung')
+    mail(to: 'berlin@bici-bike-berlin.com', subject: 'Neue Tour Buchung')
   end
 
 end
